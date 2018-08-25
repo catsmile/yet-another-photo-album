890a630f4be0915aaa1387a71a9b29ebc543e805
unknown <dmitry.kozhemyachenko@gmail.com>
2018-08-25 17:03:53 +0300

    gitignore changes

--- dist/dist/bundle.js
+++ dist/dist/bundle.js
@@ -347,46 +347,55 @@ var Main = /** @class */ (function (_super) {
                     {
                         links: [
                             {
-                                name: 'Home',
-                                url: 'http://example.com',
+                                name: "Home",
+                                url: "http://example.com",
                                 links: [
                                     {
-                                        name: 'Activity',
-                                        url: 'http://msn.com',
-                                        key: 'key1'
+                                        name: "Activity",
+                                        url: "http://msn.com",
+                                        key: "key1"
                                     },
                                     {
-                                        name: 'News',
-                                        url: 'http://msn.com',
-                                        key: 'key2'
+                                        name: "News",
+                                        url: "http://msn.com",
+                                        key: "key2"
                                     }
                                 ],
                                 isExpanded: true
                             },
-                            { name: 'Documents', url: 'http://example.com', key: 'key3', isExpanded: true },
-                            { name: 'Pages', url: 'http://msn.com', key: 'key4' },
-                            { name: 'Notebook', url: 'http://msn.com', key: 'key5' },
-                            { name: 'Long Name Test for ellipse', url: 'http://msn.com', key: 'key6' },
                             {
-                                name: 'Edit',
-                                url: 'http://cnn.com',
-                                onClick: this._onClickHandler2,
-                                icon: 'Edit',
-                                key: 'key8'
+                                name: "Documents",
+                                url: "http://example.com",
+                                key: "key3",
+                                isExpanded: true
+                            },
+                            { name: "Pages", url: "http://msn.com", key: "key4" },
+                            { name: "Notebook", url: "http://msn.com", key: "key5" },
+                            {
+                                name: "Long Name Test for ellipse",
+                                url: "http://msn.com",
+                                key: "key6"
                             },
                             {
-                                name: 'Delete',
-                                url: 'http://cnn.com',
+                                name: "Edit",
+                                url: "http://cnn.com",
                                 onClick: this._onClickHandler2,
-                                iconProps: { iconName: 'Delete' },
-                                key: 'key9'
+                                icon: "Edit",
+                                key: "key8"
+                            },
+                            {
+                                name: "Delete",
+                                url: "http://cnn.com",
+                                onClick: this._onClickHandler2,
+                                iconProps: { iconName: "Delete" },
+                                key: "key9"
                             }
                         ]
                     }
-                ], expandedStateText: 'expanded', collapsedStateText: 'collapsed', selectedKey: 'key3' })));
+                ], expandedStateText: "expanded", collapsedStateText: "collapsed", selectedKey: "key3" })));
     };
     Main.prototype._onClickHandler = function (e) {
-        alert('test');
+        alert("test");
         return false;
     };
     Main.prototype._onClickHandler2 = function (e) {
